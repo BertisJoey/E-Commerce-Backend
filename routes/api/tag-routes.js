ed8f60e0748d527f7e9eee2a03d3d4426d6c6022
BertisJoey <joeywiesner@gmail.com>
2023-08-31 19:27:50 -0400

    final commit of code

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,7 +38,9 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Tag.create(req.body);
+    const tagData = await Tag.create({
+      name: req.body.name,
+    });
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
@@ -48,11 +50,15 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update(req.body, {
-      where: {
-        id: req.params.id,
+    const tagData = await Tag.update(
+      {
+        name: req.body.name,
       },
-    })
+      {
+        where: {
+        id: req.params.id,
+        },
+      })
     res.status(200).json(tagData);
     } catch (err) {
       res.status(400).json(err);
