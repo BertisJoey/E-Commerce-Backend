7fa9d711e4a9cda136dc2eec2cef075813826d76
BertisJoey <joeywiesner@gmail.com>
2023-08-31 20:16:45 -0400

    updated post requests

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -47,7 +47,7 @@ router.post('/', async (req, res) => {
     }
   */
     try {
-      const productData = await Category.create(req.body);
+      const productData = await Product.create(req.body);
       res.status(200).json(productData);
     } catch (err) {
       res.status(400).json(err);
